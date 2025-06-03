4f8c1308bbb22a214fc6c65e96214d239b9f1940
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2025-06-04 02:07:53 +0700

    feat: remove configuration files for Next.js, PostCSS, and Tailwind CSS

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  images: {
-    domains: ["arahrumah.com"], // Add external image domains if needed later
-  },
-};
-
-export default nextConfig;
